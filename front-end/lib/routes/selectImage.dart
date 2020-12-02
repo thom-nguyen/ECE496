d231e73a71e6a6446fbec5b87b16209195fc6ea4
Chris Pua <chris.pua@mail.utoronto.ca>
2020-12-01 22:20:42 -0500

    Finished UI/UX implementation of the SelectImage screen. Need to complete uploading and camera use functionality as well as a progress screen for demo purposes

--- front-end/lib/routes/selectImage.dart
+++ front-end/lib/routes/selectImage.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../constants.dart';
 
@@ -20,29 +21,132 @@ class SelectImageScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
+          //Center Column contents vertically,
+          mainAxisAlignment: MainAxisAlignment.center,
+          //Center Column contents vertically,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            RaisedButton(
-              child: Text('Go back!'),
-              onPressed: () {
-                // Navigate back to the first screen by popping the current route off the stack.
-                Navigator.pop(context);
-              },
-              color: Color(0xFF234256),
-              textColor: Color(0xFFEFF3F6),
+            Container(
+              padding: const EdgeInsets.only(
+                left: 12.0,
+                top: 0.0,
+                bottom: 40.0,
+              ),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_rounded,
+                      color: Color(0xFF234256),
+                    ),
+                    iconSize: 60.0,
+                    onPressed: () {
+                      // TODO: check if context is null
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
             ),
-            RaisedButton(
-              child: Text('Choose a picture!'),
-              onPressed: () {
-                _openGallery();
-              },
-              color: Color(0xFF234256),
-              textColor: Color(0xFFEFF3F6),
+
+            // Back arrow
+            Container(
+              padding: const EdgeInsets.only(
+                top: 20.0,
+              ),
+              child: Text(
+                'Upload your photo!',
+                style: GoogleFonts.lato(
+                  fontSize: 55,
+                  fontWeight: FontWeight.w800,
+                  color: Color(0xFF234256),
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
-            Icon(
-              Icons.camera_alt,
-              color: Color(0xFF234256),
-              size: 24.0,
+            Container(
+              padding: const EdgeInsets.only(
+                top: 60,
+                bottom: 80,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ButtonTheme(
+                        minWidth: 150.0,
+                        height: 150.0,
+                        child: FlatButton(
+                          onPressed: () {
+                            _openGallery();
+                          },
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            side: BorderSide.none,
+                          ),
+                          child: Icon(
+                            Icons.camera_alt,
+                            color: Color(0xFFEFF3F6),
+                            size: 90.0,
+                          ),
+                          color: Color(0xFF234256),
+                        ),
+                      ),
+                      ButtonTheme(
+                        minWidth: 150.0,
+                        height: 150.0,
+                        child: FlatButton(
+                          onPressed: () {
+                            _openGallery();
+                          },
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            side: BorderSide.none,
+                          ),
+                          child: Icon(
+                            Icons.add_photo_alternate_rounded,
+                            color: Color(0xFFEFF3F6),
+                            size: 90.0,
+                          ),
+                          color: Color(0xFF234256),
+                        ),
+                      ),
+                    ],
+                  ),
+                  // Camera and Gallery labels
+                  Container(
+                    padding: const EdgeInsets.only(
+                      left: 10.0,
+                      right: 16.0,
+                      top: 12.0,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          'Camera',
+                          style: GoogleFonts.lato(
+                            fontSize: 33,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF234256),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          'Gallery',
+                          style: GoogleFonts.lato(
+                            fontSize: 33,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF234256),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
